395acd61bcde4147ded4d5d0153c33b97677e939
AllanKariuki <alankariuki24@gmail.com>
2024-05-23 16:26:25 +0300

    feat: adding check business role to b2c

--- routers/b2cRouter.js
+++ routers/b2cRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { sendb2cPayment } = require('../controllers/b2cController');
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
+const { checkRole } = require("../middleware/roleMiddleware");
 
-router.post("/b2c", protect, sendb2cPayment)
+router.post("/b2c", protect, checkRole('business'), sendb2cPayment)
 
 module.exports = router;
