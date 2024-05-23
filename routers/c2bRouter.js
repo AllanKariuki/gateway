9e3ccf24ffb0cbebb9ad6ef53e28fc9a82b6f6cc
AllanKariuki <alankariuki24@gmail.com>
2024-05-23 16:26:59 +0300

    feat: adding check customer role to c2b

--- routers/c2bRouter.js
+++ routers/c2bRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const {requestc2bPayment, processc2bPayment} = require('../controllers/c2bController');
 const router = express.Router();
+const { checkRole } = require("../middleware/roleMiddleware");
 
-router.post("/c2b/request", protect, requestc2bPayment);
-router.post("/c2b/process", protect, processc2bPayment);
+router.post("/c2b/request", protect, checkRole('customer'), requestc2bPayment);
+router.post("/c2b/process", protect, checkRole('customer'), processc2bPayment);
 
 
 module.exports = router;
